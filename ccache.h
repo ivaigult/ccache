382648847832b8540afefbc23cad8f3fd9bdb0c3
Ivan Vaigult <i.vaigult@gmail.com>
2016-07-06 08:13:06 +0000

    Fix linking issues for mingw build

--- ccache.h
+++ ccache.h
@@ -269,7 +269,7 @@ void add_exe_ext_if_no_to_fullpath(char *full_path_win_ext, size_t max_size,
 #    endif
 #    include <windows.h>
 #    define mkdir(a,b) mkdir(a)
-#    define link(src,dst) (CreateHardLink(dst,src,NULL) ? 0 : -1)
+#    define link(src,dst) (CreateHardLinkA(dst,src,NULL) ? 0 : -1)
 #    define lstat(a,b) stat(a,b)
 #    define execv(a,b) win32execute(a,b,0,-1,-1)
 #    define execute(a,b,c,d) win32execute(*(a),a,1,b,c)
@@ -279,8 +279,12 @@ void add_exe_ext_if_no_to_fullpath(char *full_path_win_ext, size_t max_size,
 #    define PATH_DELIM ";"
 #    define F_RDLCK 0
 #    define F_WRLCK 0
+#    ifndef S_ISDIR
 #    define S_ISDIR(m) ( ((m) & _S_IFDIR) != 0)
+#    endif
+#    ifndef S_ISREG
 #    define S_ISREG(m) 0
+#    endif
 #else
 #    define DIR_DELIM_CH '\\'
 #    define PATH_DELIM ":"
